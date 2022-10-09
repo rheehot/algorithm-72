48a6662a92fd939ae8bf783f66462674b95d9a98
e-juhee <juhee971204@naver.com>
2022-10-10 00:37:52 +0900

    동전 0

--- baekjoon/11047.js
+++ baekjoon/11047.js
@@ -1,4 +1,15 @@
 const fs = require("fs");
 // const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 const input = fs.readFileSync("./11047.txt").toString().trim().split("\n");
-console.log(input);
+let price = Number(input[0].split(" ")[1]);
+let count = 0;
+for (let i = input.length - 1; i > 0; i--) {
+  if (price === 0) break;
+  const coin = Number(input[i]);
+  if (coin > price) {
+    continue;
+  }
+  count += Math.floor(price / coin);
+  price = price % coin;
+}
+console.log(count);
