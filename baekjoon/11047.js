b29197e4ff0957fa4ec8c269231560d6071dfa97
e-juhee <juhee971204@naver.com>
2022-10-09 22:25:46 +0900

    동전 0

---
+++ baekjoon/11047.js
@@ -0,0 +1,4 @@
+const fs = require("fs");
+// const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = fs.readFileSync("./11047.txt").toString().trim().split("\n");
+console.log(input);
