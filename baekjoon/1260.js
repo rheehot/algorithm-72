8f2bdd7d0678a3a4a316d69458ad4477b25b13ca
e-juhee <juhee971204@naver.com>
2022-10-23 15:14:03 +0900

    bfs dfs

--- baekjoon/1260.js
+++ baekjoon/1260.js
@@ -1,82 +1,70 @@
 const fs = require("fs");
 // const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 // const input = fs.readFileSync("./1260.txt").toString().trim().split("\n");
+const input = `
+5 5 1
+1 2
+2 3
+3 1
+1 4
+3 5
+`
+  .toString()
+  .trim()
+  .split("\n");
 
+const vertexLength = Number(input[0].split(" ")[0]);
+const degeLength = Number(input[0].split(" ")[1]);
 const start = Number(input[0].split(" ")[2]);
-const length = Number(input[0].split(" ")[0]);
-const line = Number(input[0].split(" ")[1]);
 const nodeArr = [];
 input.shift();
-
 input.forEach((el) => {
-  const node = el.split(" ");
-  if (nodeArr[node[0]]) {
-    nodeArr[node[0]] = [...nodeArr[node[0]], Number(node[1])];
-    nodeArr[node[0]].sort();
-  } else nodeArr[node[0]] = [Number(node[1])];
-  if (nodeArr[node[1]]) {
-    nodeArr[node[1]] = [...nodeArr[node[1]], Number(node[0])];
-    nodeArr[node[1]].sort();
-  } else nodeArr[node[1]] = [Number(node[0])];
+  const left = Number(el.split(" ")[0]);
+  const right = Number(el.split(" ")[1]);
+  if (!nodeArr[left]) nodeArr[left] = [right];
+  else nodeArr[left] = [...nodeArr[left], right];
+  if (!nodeArr[right]) nodeArr[right] = [left];
+  else nodeArr[right] = [...nodeArr[right], left];
 });
+nodeArr.map((el) => el.sort());
 
-const bfs = [start];
-const dfs = [start];
-
-new Array(length).fill("").forEach((_, i) => {
-  if (nodeArr[bfs[i]])
-    nodeArr[bfs[i]].forEach((el) => {
-      if (bfs.includes(el)) return;
-      bfs.push(el);
-    });
-});
-
-console.log(nodeArr);
-new Array(length).fill("").forEach((_) => {
-  // 현재 점
-  const current = dfs[dfs.length - 1];
-  console.log(current);
-  // 현재 점과 연결된 점
-  for (let i = 0; i < nodeArr[current].length; i++) {
-    const el = nodeArr[current][i];
-    console.log(el);
-    // 이미 포함하고 있으면
-    if (dfs.includes(el)) continue;
-    // 가장 작은 점 추가
-    else {
-      dfs.push(el);
-      break;
-    }
-  }
-});
-// 현재 점과 연결된 점 중에서 가지 않은 점 찾기
-const dfsFunc = (current) => {
-  let next;
-  for (let i = 0; i < nodeArr[current].length; i++) {
-    // 이미 갔으면 undefined
-    if (dfs.includes(nodeArr[current][i])) continue;
-    // 가지 않은 점
-    next = nodeArr[current][i];
-  }
-
-  // 가지 않은 점을 찾았으면 리턴
-  if (next) {
-    return next;
-  } else {
-    nodeArr[current].forEach((el) => {
-      dfsFunc(el);
+// bfs
+const bfs = () => {
+  const check = [];
+  const result = [start];
+  const queue = [start];
+  while (queue.length) {
+    const current = queue.shift();
+    // 이미 확인했으면
+    if (check[current]) return;
+    check[current] = true;
+    nodeArr[current].forEach((vertex) => {
+      if (result.includes(vertex)) return;
+      result.push(vertex);
+      queue.push(vertex);
     });
   }
+  return result;
 };
-console.log(line + 1, dfs.length);
 
-while (dfs.length !== length) {
-  // 마지막 점(현재 점)
-  let current = dfs[dfs.length - 1];
-  //   const result = dfsFunc(current);
-  //   if (result ) dfs.push(result);
-  dfs.push("A");
-}
-
-console.log(...dfs);
-console.log(...bfs);
+// dfs
+const check = [];
+const result = [start];
+const dfs = () => {
+  current = result[result.length - 1];
+  if (check[current]) return;
+  check[current] = true;
+  for (let i = 0; i < nodeArr[current].length; i++) {
+    const vertex = nodeArr[current][i];
+    if (result.includes(vertex)) continue;
+    result.push(vertex);
+    break;
+  }
+  dfs();
+};
+dfs();
+console.log(nodeArr);
+console.log(...result);
+// 1 2 3 5 4
+console.log(...bfs(start));
+// 1 2 3 4 5
