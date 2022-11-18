d84467392c88cebfc5ce19fb5b78dd8b679eb025
e-juhee <juhee971204@naver.com>
2022-11-18 19:29:27 +0900

    baekjoon 1676

--- baekjoon/1676.js
+++ baekjoon/1676.js
@@ -1,14 +1,10 @@
 const fs = require("fs");
 // const input = fs.readFileSync("/dev/stdin").toString().trim();
 const input = fs.readFileSync("./1676.txt").toString().trim();
-let factorial = 1;
-new Array(Number(input)).fill(1).forEach((el, i) => {
-  factorial *= el + i;
-});
-let result = 0;
-for (let i = String(factorial).length - 1; i >= 1; i--) {
-  if (String(factorial)[i] === "0") {
-    result++;
-  } else break;
+let result = ~~(input / 5);
+let count = result;
+while (result >= 5) {
+  result = ~~(result / 5);
+  count += result;
 }
-console.log(result);
+console.log(count);
