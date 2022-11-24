834d63c69b1a7c130ebd109f3000a136b150b756
e-juhee <juhee971204@naver.com>
2022-11-23 22:16:55 -0500

    algorithm

---
+++ study/section1/1018.js
@@ -0,0 +1,27 @@
+function solution5(n) {
+  return Math.ceil(n / 12);
+}
+function solution1(n) {
+  return n % 12 ? ~~(n / 12) + 1 : n / 12;
+}
+function solution2(n) {
+  return n % 12 ? parseInt(n / 12) + 1 : n / 12;
+}
+function solution4(n) {
+  const str = String(n / 12).split(".");
+  return str[1] ? Number(str[0]) + 1 : Number(str[0]);
+}
+function solution3(n) {
+  if (n % 12 === 0) return n / 12;
+  let int = "";
+  for (let i = 0; i < String(n / 12).length - 1; i++) {
+    const str = String(n / 12)[i];
+    if (str === ".") break;
+    int += str;
+  }
+  return Number(int) + 1;
+}
+
+console.log(solution(178)); // 15
+console.log(solution(25)); // 3
+console.log(solution(36)); // 3
---
+++ study/section1/1124-3-중복단어제거.js
@@ -0,0 +1,12 @@
+function solution(input) {
+  const result = [];
+  for (el of input) {
+    if (result.includes(el)) {
+      continue;
+    }
+    result.push(el);
+  }
+  return result;
+}
+
+console.log(solution(["good", "time", "good", "time", "student"]));
