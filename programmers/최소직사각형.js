4fd2788ba42036ec42b7b4f17b5b48b4c96cb429
e-juhee <juhee971204@naver.com>
2022-12-10 18:44:26 +0900

    최소직사각형

--- programmers/최소직사각형.js
+++ programmers/최소직사각형.js
@@ -1,41 +1,9 @@
 function solution(sizes) {
-  const width = Math.max(...sizes.map((el) => el[0]));
-  const height = Math.max(...sizes.map((el) => el[1]));
-  let result = width * height;
-  const getCardList = (arr, index, width, height) => {
-    if (sizes.length <= index) {
-      if (result > width * height) result = width * height;
-    } else {
-      if (
-        sizes[index][0] < width &&
-        sizes[index][0] < height &&
-        sizes[index][1] < width &&
-        sizes[index][1] < height
-      ) {
-        return getCardList([...arr], index + 1, width, height);
-      }
-      [0, 1].forEach((el) => {
-        const newCard = el ? [sizes[index][1], sizes[index][0]] : sizes[index];
-
-        const temp = [...arr, newCard];
-
-        return getCardList(
-          temp,
-          index + 1,
-          newCard[0] > width ? newCard[0] : width,
-          newCard[1] > height ? newCard[1] : height
-        );
-      });
-    }
-  };
-  getCardList([sizes[0]], 1, sizes[0][0], sizes[0][1]);
-  return result;
+  const big = [];
+  const small = [];
+  sizes.map((el) => {
+    big.push(Math.max(...el));
+    small.push(Math.min(...el));
+  });
+  return Math.max(...big) * Math.max(...small);
 }
-
-solution([
-  [60, 50],
-  [30, 70],
-  [60, 30],
-  [80, 40],
-]);
-// 4000
