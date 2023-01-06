a4c08f4c7b616826147c24f21ab7b265830dc6a3
e-juhee <juhee971204@naver.com>
2023-01-07 02:35:01 +0900

    algorithm

---
+++ baekjoon/10926.js
@@ -0,0 +1,4 @@
+const fs = require("fs");
+// const input = fs.readFileSync("/dev/stdin").toString().trim();
+const input = fs.readFileSync("./10926.txt").toString().trim();
+console.log(input + "??!");
